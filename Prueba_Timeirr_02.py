824da80eb4bd0bfcc5d4803141aa6f27e7cea6e3
alexeal90 <alexeal90@gmail.com>
2017-10-17 19:36:57 +0200

    arreglo codigo

--- Prueba_Timeirr_02.py
+++ Prueba_Timeirr_02.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 11 12:51:31 2017
-
-@author: Alex
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Wed Sep 27 18:37:16 2017
 
 @author: Alex
@@ -20,6 +13,51 @@ import numpy as np
 import scipy as sc
 
 
+hrv = HRV_entropy() #create class of hrv entropy
+
+asi = [[0 for x in range(10)] for y in range(9)]
+
+tau  = 10
+N = 300*tau
+for i in range(10):
+    Mix1 = mix(N,0.1)
+    Mix2 = mix(N,0.2)
+    Mix3 = mix(N,0.3)
+    Mix4 = mix(N,0.4)
+    Mix5 = mix(N,0.5)
+    Mix6 = mix(N,0.6)
+    Mix7 = mix(N,0.7)
+    Mix8 = mix(N,0.8)
+    Mix9 = mix(N,0.9)
+    
+    asi_aux = np.arange(9)
+
+    asi_aux[0],tirr_1 = hrv.TimeIrreversibility(Mix1[:,0],tau)
+    asi_aux[1],tirr_2 = hrv.TimeIrreversibility(Mix2[:,0],tau)
+    asi_aux[2],tirr_3 = hrv.TimeIrreversibility(Mix3[:,0],tau)
+    asi_aux[3],tirr_4 = hrv.TimeIrreversibility(Mix4[:,0],tau)
+    asi_aux[4],tirr_5 = hrv.TimeIrreversibility(Mix5[:,0],tau)
+    asi_aux[5],tirr_6 = hrv.TimeIrreversibility(Mix6[:,0],tau)
+    asi_aux[6],tirr_7 = hrv.TimeIrreversibility(Mix7[:,0],tau)
+    asi_aux[7],tirr_8 = hrv.TimeIrreversibility(Mix8[:,0],tau)
+    asi_aux[8],tirr_9 = hrv.TimeIrreversibility(Mix9[:,0],tau)
+    
+    for j in range(9):
+        asi[j][i] = asi_aux[j]
+
+
+asi_mean = np.arange(9)
+asi_std = np.arange(9)
+for k, l in enumerate(asi):
+    asi_mean[k] = np.mean(asi[k])
+    asi_std[k] = np.std(asi[k])
+
+P = np.arange(0.1, 1, 0.1)
+for Each_P in range(9):
+    plt.errorbar(P[Each_P],asi_mean[Each_P],yerr = asi_std[Each_P],fmt = 'o-')
+
+#%% Antiguo codigo
+'''
 hrv = HRV_entropy() #create class of hrv entropy
 
 Timeirr_01 = []
@@ -102,14 +140,13 @@ t08_m = np.mean(asi_08)
 t08_std = np.std(asi_08)
 t09_m = np.mean(asi_09)
 t09_std = np.std(asi_09)
-
-plt.errorbar(0.1,t01_m,yerr = t01_std,fmt = 'o-')
-plt.errorbar(0.2,t02_m,yerr = t02_std,fmt = 'o-')
-plt.errorbar(0.3,t03_m,yerr = t03_std,fmt = 'o-')
-plt.errorbar(0.4,t04_m,yerr = t04_std,fmt = 'o-')
-plt.errorbar(0.5,t05_m,yerr = t05_std,fmt = 'o-')
-plt.errorbar(0.6,t06_m,yerr = t06_std,fmt = 'o-')
-plt.errorbar(0.7,t07_m,yerr = t07_std,fmt = 'o-')
-plt.errorbar(0.8,t08_m,yerr = t08_std,fmt = 'o-')
-plt.errorbar(0.9,t09_m,yerr = t09_std,fmt = 'o-')
-
+plt.errorbar(0.1,asi_mean[0],yerr = asi_std[0],fmt = 'o-')
+plt.errorbar(0.2,asi_mean[1],yerr = asi_std[1],fmt = 'o-')
+plt.errorbar(0.3,asi_mean[2],yerr = asi_std[2],fmt = 'o-')
+plt.errorbar(0.4,asi_mean[3],yerr = asi_std[3],fmt = 'o-')
+plt.errorbar(0.5,asi_mean[4],yerr = asi_std[4],fmt = 'o-')
+plt.errorbar(0.6,asi_mean[5],yerr = asi_std[5],fmt = 'o-')
+plt.errorbar(0.7,asi_mean[6],yerr = asi_std[6],fmt = 'o-')
+plt.errorbar(0.8,asi_mean[7],yerr = asi_std[7],fmt = 'o-')
+plt.errorbar(0.9,asi_mean[8],yerr = asi_std[8],fmt = 'o-')
+'''
